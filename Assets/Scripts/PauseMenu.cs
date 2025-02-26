1256f5657bdf4d5540450c902af2be5ea2f0729d
Filip Cieśla <166231501+Ajdacho@users.noreply.github.com>
2025-02-26 21:36:50 +0100

    Add options menu.
    - Switch between pause menu and options menu.
    - Volume slider to adjust sound levels.
    - Dropdown menu for selecting screen resolution.
    - Checkbox for toggling fullscreen mode.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                gameResume();
+            }
+            else
+            {
+                gamePause();
+            }
+        }
+    }
+
+    public void gameResume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void gamePause()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void gameLoadSettings()
+    {
+
+    }
+
+    public void gameExit()
+    {
+        Debug.Log("guwnoguwnoguwno");
+        Application.Quit();
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void setResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
+
